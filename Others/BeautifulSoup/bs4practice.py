d1d76c435c3bb0abeb474e85edf3609ca4493419
DESKTOP-FU91PJ0\hyunjun son <jjang3a@gmail.com>
2022-05-29 17:29:52 +0900

    bs4 & selenium add

---
+++ Others/BeautifulSoup/bs4practice.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome('chromedriver')
+driver.get('http://naver.com')
+
+soup = BeautifulSoup(driver.page_source)
+print(soup.select('div'))
+# driver.execute_script('alert("test")')
+# driver.find_element_by_class_name('link_login').click()
+# driver.implicitly_wait(1)
+# driver.find_element_by_css_selector('#id').send_keys('jjang2a')
+# driver.find_element_by_css_selector('#pw').send_keys('tt4939gnn@')
+# driver.implicitly_wait(1)
+# driver.find_element_by_css_selector('.btn_login').click()
+# print('aa')
+
+
+
