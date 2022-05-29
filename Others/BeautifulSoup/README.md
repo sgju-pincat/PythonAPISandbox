bf952776cb4caf7b4eda99f57890e754b095ca24
sugang.ju <sugang.ju@naverz-corp.com>
2022-05-29 17:44:57 +0900

    temp

---
+++ Others/BeautifulSoup/README.md
@@ -0,0 +1,6 @@
+beautifulsoup를 사용하는 예제폴더입니다.
+
+사용방법
+pip install bs4
+pip install selenium
+
--- Others/BeautifulSoup/bs4practice.py
+++ Others/BeautifulSoup/bs4practice.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome('chromedriver')
+driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.get('http://naver.com')
 
 soup = BeautifulSoup(driver.page_source)
@@ -11,8 +11,8 @@ print(soup.select('div'))
 # driver.execute_script('alert("test")')
 # driver.find_element_by_class_name('link_login').click()
 # driver.implicitly_wait(1)
-# driver.find_element_by_css_selector('#id').send_keys('jjang2a')
-# driver.find_element_by_css_selector('#pw').send_keys('tt4939gnn@')
+# driver.find_element_by_css_selector('#id').send_keys('')
+# driver.find_element_by_css_selector('#pw').send_keys('')
 # driver.implicitly_wait(1)
 # driver.find_element_by_css_selector('.btn_login').click()
 # print('aa')
